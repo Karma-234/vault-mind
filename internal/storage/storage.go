d2fb9d4feeea9c38b7c9799a578c7b35a9f38202
Karma-234 <busari1kamal@gmail.com>
2026-03-04 23:12:07 +0100

    Add: db initialize

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,32 @@
 package storage
+
+import (
+	"time"
+
+	"github.com/cockroachdb/pebble"
+)
+
+type Credential struct {
+	ID      string    `json:"id"`
+	Service string    `json:"service"`
+	Type    string    `json:"type"`
+	Notes   string    `json:"notes,omitempty"`
+	Created time.Time `json:"createdAt"`
+	Updated time.Time `json:"updatedAt,omitempty"`
+}
+type Storage interface {
+	AddCredential(service, credType, secret, notes string) (string, error)
+	ListCredentials() ([]Credential, error)
+	GetCredential(id string) (*Credential, string, error)
+}
+
+type VaultPebbleStorage struct {
+	Storage
+	db  *pebble.DB
+	key []byte
+}
+
+func (s *VaultPebbleStorage) AddCredential(service, credType, secret, notes string) (string, error) {
+
+	return "", nil
+}
